6bc77610f11b7dc2be2888a06232832857cc46b6
Dropje97 <83907974+Dropje97@users.noreply.github.com>
2022-05-06 16:11:57 +0200

    Update temp_motor_meten.ino

--- temp_motor_meten/temp_motor_meten/temp_motor_meten.ino
+++ temp_motor_meten/temp_motor_meten/temp_motor_meten.ino
@@ -15,7 +15,9 @@ door bv. door alleen te gaan meten wanneer de trottle recentelijk over de 30% is
 3. de schoef moet compleet stil staan anders produceert die zelf stroom als een dynamo.
 */
 
-RunningAverage myRA(80);
+RunningAverage myRA(100);
+RunningAverage myRA_min(60);
+RunningAverage myRA_s(9);
 
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 32  // OLED display height, in pixels
@@ -38,53 +40,52 @@ enum class measurmentState : uint8_t {
   COOLDOWN,         // defaults to 5, wait 1s for lm317 cool down, check permission wile waiting (if permission start load, else disconntect motor and idle)
   // stop loop after 10min consecutively measurments
 };
-measurmentState currState = measurmentState::IDLE; // Keep track of the current State (it's a measurmentState variable)
+measurmentState currState = measurmentState::IDLE;  // Keep track of the current State (it's a measurmentState variable)
 
-const int8_t READY_PIN = 13; // Pin connected to the ALERT/RDY signal for new sample notification.
+const int8_t READY_PIN = 13;  // Pin connected to the ALERT/RDY signal for new sample notification.
+const int8_t LOAD_PIN = 27; // Pin to turn the 1A load on
 
-const float multiplier = 0.0078125F; /* ADS1115  @ +/- +/- 0.256V (16-bit results). Be sure to update this value based on the IC and the gain settings! */
-const float referenceTemperature = 20; // temperature when the referenceVoltagemV was measured
-const float referenceVoltagemV = 1; // voltage between the motor windings at the referenceTemperature
-const float temperatureCoefficient = 2; // 
+const float multiplier = 0.0078125F;     /* ADS1115  @ +/- +/- 0.256V (16-bit results). Be sure to update this value based on the IC and the gain settings! */
+const float referenceTemperature = 20;   // temperature when the referenceVoltagemV was measured
+const float referenceVoltagemV = 1;      // voltage between the motor windings at the referenceTemperature
+const float temperatureCoefficient = 2;  //
 
-float motorTemperature = 0; // current calculated temperture ftom the resistance of the motor windings
-float measurmentRawAvg = 0; 
+float motorTemperature = 0;  // current calculated temperture ftom the resistance of the motor windings
+float measurmentRawAvg = 0;
 
-bool trottlePermission = false;  // permission from trottle to take a measurment
+bool trottlePermission = true;  // permission from trottle to take a measurment
 bool lastTrottlePermission = trottlePermission;
-bool motorConnected = false;     // 1A load and ads1115 connected to motor
-bool tenMinCoolDown = false;     // stop taking measurments after 10min consecutively measuring
-bool chargeBattery = false;      // charge battery when in idle 
-bool currentSourceOn = false;    // current for measuring temperature =  of motor
+bool motorConnected = false;   // 1A load and ads1115 connected to motor
+bool tenMinCoolDown = false;   // stop taking measurments after 10min consecutively measuring
+bool chargeBattery = false;    // charge battery when in idle
+bool currentSourceOn = false;  // current for measuring temperature =  of motor
 
 uint32_t tenMinCoolDownTime = millis();
 int16_t measurementRaw = 0;
 float voltagemV = 0;
 
-uint8_t broadcastAddress[] = {0x9C,0x9C,0x1F,0xDD,0x4D,0xD0}; // MAC Address of receiver esp32 (own MAC Address: AC:67:B2:36:B2:D0) 
-String success; // Variable to store if sending data was successful
-esp_now_peer_info_t peerInfo; // store info of throttle esp
+uint8_t broadcastAddress[] = { 0x9C, 0x9C, 0x1F, 0xDD, 0x4D, 0xD0 };  // MAC Address of receiver esp32 (own MAC Address: AC:67:B2:36:B2:D0)
+String success;                                                       // Variable to store if sending data was successful
+esp_now_peer_info_t peerInfo;                                         // store info of throttle esp
 
 // This is required on ESP32 to put the ISR in IRAM. Define as
 // empty for other platforms. Be careful - other platforms may have
 // other requirements.
-#ifndef IRAM_ATTR 
-#define IRAM_ATTR 
+#ifndef IRAM_ATTR
+#define IRAM_ATTR
 #endif
 
 // Callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   //Serial.print("\r\nLast Packet Send Status:\t");
   //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  
 }
 
 // Callback when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   memcpy(&trottlePermission, incomingData, sizeof(trottlePermission));
   //Serial.print("Bytes received: ");
   //Serial.println(len);
-  
 }
 
 // Forward declaration of all functions
@@ -108,17 +109,17 @@ void setup(void) {
     return;
   }
 
-// Once ESPNow is successfully Init, we will register for Send CB to
+  // Once ESPNow is successfully Init, we will register for Send CB to
   // get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
 
- // Register peer
+  // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
+  peerInfo.channel = 0;
   peerInfo.encrypt = false;
-  
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     //Serial.println("Failed to add peer");
     return;
   }
@@ -162,10 +163,11 @@ void setup(void) {
     Serial.println(F("Failed to initialize ADS."));
   }
 
+  pinMode(LOAD_PIN, OUTPUT);
   pinMode(READY_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(READY_PIN), NewDataReadyISR, FALLING);  // We get a falling edge every time a new sample is ready.
 
-  ads.startADCReading(ADS1X15_REG_CONFIG_MUX_DIFF_0_1, /*continuous=*/true);     // Start continuous conversions.
+  ads.startADCReading(ADS1X15_REG_CONFIG_MUX_DIFF_0_1, /*continuous=*/true);  // Start continuous conversions.
 }
 
 void loop(void) {
@@ -174,84 +176,88 @@ void loop(void) {
 
     // Initial state (or final returned state)
     case measurmentState::IDLE:
-    displayState("IDLE state");  // todo: voeg dit overal toe. mischien ifdef debug?
+      displayState("IDLE state");  // todo: voeg dit overal toe. mischien ifdef debug?
 
-    if((!trottlePermission || tenMinCoolDown) && motorConnected) {
-      // todo: disconntectMotor()
-      motorConnected = false;
-    }
-    if((!trottlePermission || tenMinCoolDown) && !motorConnected) {
-      sendMotorState();
-      // todo: sendMotorState() (motorConnected true or false) to trottle with ESPNOW The boat is allowed to seal!
-    }
-    if((!trottlePermission || tenMinCoolDown) && !chargeBattery) {
-      // todo: turnOnBattery()
-      chargeBattery = true;
-    }
+      if ((!trottlePermission || tenMinCoolDown) && motorConnected) {
+        // todo: disconntectMotor()
+        motorConnected = false;
+      }
+      if ((!trottlePermission || tenMinCoolDown) && !motorConnected) {
+        sendMotorState();
+        // todo: sendMotorState() (motorConnected true or false) to trottle with ESPNOW The boat is allowed to seal!
+      }
+      if ((!trottlePermission || tenMinCoolDown) && !chargeBattery) {
+        // todo: turnOnBattery()
+        chargeBattery = true;
+      }
 
-    // reset tenMinCoolDown if trottlePermission changes 
-    if(trottlePermission != lastTrottlePermission) {
-      tenMinCoolDown = false;
-      lastTrottlePermission = trottlePermission;
-    }
+      // reset tenMinCoolDown if trottlePermission changes
+      if (trottlePermission != lastTrottlePermission) {
+        tenMinCoolDown = false;
+        lastTrottlePermission = trottlePermission; 
+      }
 
-    // if allowed prepare for measurment
-    if(trottlePermission && !tenMinCoolDown) {
-      if(chargeBattery) {
-        // todo: turnOffBattry()
-        chargeBattery = false;
+      // if allowed prepare for measurment
+      if (trottlePermission && !tenMinCoolDown) {
+        if (chargeBattery) {
+          // todo: turnOffBattry()
+          chargeBattery = false;
+        }
+        if (!motorConnected) {
+          // todo: connectMotor()
+          motorConnected = true;
+        }
+        // if everything is ready, start the cooldown timer, the load and tell the trottle we are measuring
+        if (!chargeBattery && motorConnected) {
+          // todo: sendMotorState() (motorConnected true or false) to trottle with ESPNOW The boat is not allowed to seal!
+          currState = measurmentState::STARTLOAD;
+          tenMinCoolDownTime = millis();  // reset 10 min cooldown timer
+        }
       }
-      if(!motorConnected) {
-        // todo: connectMotor()
-        motorConnected = true;
-      }
-      // if everything is ready, start the cooldown timer, the load and tell the trottle we are measuring
-      if(!chargeBattery && !motorConnected) {
-        // todo: sendMotorState() (motorConnected true or false) to trottle with ESPNOW The boat is not allowed to seal!
-        currState = measurmentState::STARTLOAD;
-      tenMinCoolDownTime = millis(); // reset 10 min cooldown timer
-      }
-    }
 
       break;
 
     case measurmentState::STARTLOAD:
-    static uint32_t currentSourceOnTime = millis();
-    const static int8_t currentSourceOnDelay = 20;    // wait 20ms for current to settle (unsure if 20ms is enough)
+      static uint32_t currentSourceOnTime = millis();
+      const static int8_t currentSourceOnDelay = 100;  // wait 20ms for current to settle (unsure if 20ms is enough)
 
-    if (!currentSourceOn) {
-      // todo: turnOnCurrentSource();
-      currentSourceOn = true;
-    }
-    // wait ...ms for current to settle
-    if(currentSourceOn) {
-      if(millis() - currentSourceOnTime >= currentSourceOnDelay) {
-      currState = measurmentState::TAKEMEASUREMENT;
+      if (!currentSourceOn) {
+        digitalWrite(LOAD_PIN, HIGH);
+        currentSourceOnTime = millis();
+        currentSourceOn = true;
       }
-    }
- 
+      // wait ...ms for current to settle
+      if (currentSourceOn) {
+        if (millis() - currentSourceOnTime >= currentSourceOnDelay) {
+          currState = measurmentState::TAKEMEASUREMENT;
+        }
+      }
+
       break;
 
     case measurmentState::TAKEMEASUREMENT:
-    // todo: na te veel tijd geen meting hebben gehad stop met meten zodat de boot weer kan varen en stuur een error. misschien een negative temperatuur als error?
+      // todo: na te veel tijd geen meting hebben gehad stop met meten zodat de boot weer kan varen en stuur een error. misschien een negative temperatuur als error?
 
-    static int16_t i = 0;
-    if(newMeasurment && (i <= 1000)) { // wait until there is an new measurement and stop after ... measurments
-      newMeasurment = false;       // reset newMeasurement
-      measurementRaw = ads.getLastConversionResults();
-    #ifdef DEBUG
-    Serial.print(i); Serial.print("\t"); Serial.println(measurementRaw);
-    #endif   
-      // todo: add measurementRaw to measurmentRawAvg?? float problem? solution first calculate temperature = ? I don't know
-      i++; // add 1 to the measurement counter
-    }
-    if(i > 80) {
-      i = 0;
-      currState = measurmentState::STOPLOAD;
-      #ifdef DEBUG
-      Serial.println(i);
-      #endif   
-    }
+      static int16_t i = 0;
+      if (newMeasurment && (i < 100)) {  // wait until there is an new measurement and stop after ... measurments
+        newMeasurment = false;             // reset newMeasurement
+        measurementRaw = ads.getLastConversionResults();
+        myRA.addValue(measurementRaw);
+#ifdef DEBUG
+        Serial.print(i);
+        Serial.print("\t");
+        Serial.println(measurementRaw);
+#endif
+        // todo: add measurementRaw to measurmentRawAvg?? float problem? solution first calculate temperature = ? I don't know
+        i++;  // add 1 to the measurement counter
+      }
+      if (i >= 100) {
+        i = 0;
+        currState = measurmentState::STOPLOAD;
+#ifdef DEBUG
+        Serial.println(i);
+#endif
+      }
 
 
 
@@ -259,71 +265,81 @@ void loop(void) {
 
     case measurmentState::STOPLOAD:
 
-    if (currentSourceOn) {
-      // todo: turnOffCurrentSource();
-      currentSourceOn = false;
-    }
-    if(!currentSourceOn) {
-      currState = measurmentState::SENDRESULT;
-    }
+      if (currentSourceOn) {
+        digitalWrite(LOAD_PIN, LOW);
+        currentSourceOn = false;
+      }
+      if (!currentSourceOn) {
+        currState = measurmentState::SENDRESULT;
+      }
 
       break;
 
     case measurmentState::SENDRESULT:
-    // todo: get.avg = measurmentRawAvg;
+      measurmentRawAvg = myRA.getAverage();
+      myRA_min.addValue(measurmentRawAvg);
+      static float minAvg2 = 0;
+      static float secAvg = 0;
+      minAvg2 = myRA_min.getAverage();
+      secAvg = myRA_s.getAverage();
 
-    motorTemperature = (measurmentRawAvg - referenceVoltagemV) / referenceVoltagemV * temperatureCoefficient + referenceTemperature;
-    Serial.println(motorTemperature);
-    // todo: send mqtt motorTemperature and measurmentRawAvg
-    // todo: send espnow motorTemperature to fancy display
-    // 
+      motorTemperature = (measurmentRawAvg - referenceVoltagemV) / referenceVoltagemV * temperatureCoefficient + referenceTemperature;
+      //Serial.println(motorTemperature);
+      // todo: send mqtt motorTemperature and measurmentRawAvg
+      // todo: send espnow motorTemperature to fancy display
+      //
+
+      display.clearDisplay();
+      display.setTextSize(1);  // Draw 2X-scale text
+      display.setTextColor(SSD1306_WHITE);
+      display.setCursor(0, 0);  // Start at top-left corner
+      display.print(measurmentRawAvg*multiplier, 3);
+      display.setCursor(0, 12);  // Start at top-left corner
+      display.print(secAvg*multiplier, 3);
+      display.setCursor(0, 24);
+      display.print(minAvg2*multiplier, 3);
+      display.display();
+
+      Serial.println(measurmentRawAvg*multiplier, 3);
+
+      currState = measurmentState::COOLDOWN;
+      myRA.clear();
 
       break;
 
     case measurmentState::COOLDOWN:
-
-
+    // todo: add permission check and 10mincooldown
+    if(millis() - currentSourceOnTime > 1000) {
+      currState = measurmentState::STARTLOAD;
+    }
       break;
-
   }
 
   //measurementcasmeasurementRaw * multiplier;
-  Serial.print("Differential: ");
-  Serial.print(measurementRaw);
-  Serial.print("(");
-  Serial.print(voltagemV);
-  Serial.println("mV)");
 
-  display.clearDisplay();
-  display.setTextSize(2);  // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);  // Start at top-left corner
-  display.print(voltagemV, 3);
-  display.display();
-  delay(2000);
-  
+  //Serial.print(measurementRaw);
 }
 
 void sendMotorState() {
   // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &motorConnected, sizeof(motorConnected));
-   
- #ifdef DEBUG 
- /* if (result == ESP_OK) {
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&motorConnected, sizeof(motorConnected));
+
+#ifdef DEBUG
+  /* if (result == ESP_OK) {
     Serial.println("Sent with success");
   }
   else {
     Serial.println("Error sending the data");
   } */
-  #endif 
+#endif
 }
 
 // Helper routine to track state machine progress
 void displayState(String currState) {
-    static String prevState = "";
+  static String prevState = "";
 
-    if (currState != prevState) {
-        Serial.println(currState);
-        prevState = currState;
-    }
+  if (currState != prevState) {
+    Serial.println(currState);
+    prevState = currState;
+  }
 }
